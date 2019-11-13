01df51d4a0fe34514bac037a0252ef9ed38a9e19
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-13 21:26:21 +0530

    Create Java Currency Formatter.java

---
+++ JAVA/Java Currency Formatter.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        
+        Locale INDIA = new Locale("en", "IN");
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat india = NumberFormat.getCurrencyInstance(INDIA);
+
+ 
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
+
