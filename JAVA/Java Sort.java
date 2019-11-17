17834d2fc366eda8cee6a398688c8b41c344b127
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-17 17:46:14 +0530

    Update Java Sort.java

--- JAVA/Java Sort.java
+++ JAVA/Java Sort.java
@@ -6,22 +6,29 @@ class Student {
 	private int id;
 	private String fname;
 	private double cgpa;
-	public Student(int id, String fname, double cgpa) {
+	public Student(int id, String fname, double cgpa)
+	{
 		super();
 		this.id = id;
 		this.fname = fname;
 		this.cgpa = cgpa;
+		
 	}
-	public int getId() {
+	public int getId() 
+	{
 		return id;
 	}
-	public String getFname() {
+	public String getFname() 
+	{
 		return fname;
 	}
-	public double getCgpa() {
+	public double getCgpa()
+	{
 		return cgpa;
 	}
+	
 }
+
 
 
 public class Solution
