aa6814a104fb1e8058b6eb703b413f323b1c9a39
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-17 17:47:27 +0530

    Update Pattern Syntax Checker.java

--- JAVA/Pattern Syntax Checker.java
+++ JAVA/Pattern Syntax Checker.java
@@ -4,13 +4,13 @@ import java.util.regex.*;
 public class Solution
 {
 	public static void main(String[] args)
-  {
+ 	 {
 		Scanner in = new Scanner(System.in);
     
 		int testCases = Integer.parseInt(in.nextLine());
     
 		while(testCases>0)
-    {
+  		{
 			String pattern = in.nextLine();
           	
                 
@@ -20,6 +20,7 @@ public class Solution
                   System.out.println("Valid");
 
               }
+			
               catch(PatternSyntaxException e){
                   System.out.println("Invalid");
               }
