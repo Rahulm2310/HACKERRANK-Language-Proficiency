5069dbb7720950d3cad9476bcc2a211bda9e406f
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-19 18:25:40 +0530

    Create BigInteger.java

---
+++ JAVA/BigInteger.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        
+        BigInteger a1=new BigInteger(a);
+        BigInteger b1=new BigInteger(b);
+
+        System.out.println(a1.add(b1));
+        System.out.println(a1.multiply(b1));
+
+
+    }
+}
+
