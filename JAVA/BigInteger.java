d2a1dc656e6f9b29e1822936936521501a01ae39
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-23 22:26:35 +0530

    Update BigInteger.java

--- JAVA/BigInteger.java
+++ JAVA/BigInteger.java
@@ -4,7 +4,8 @@ import java.math.BigInteger;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+ {
         
         Scanner sc=new Scanner(System.in);
         String a=sc.nextLine();
