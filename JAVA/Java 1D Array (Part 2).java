b0fa531c6bcced62659d861506b1b4bdd28f04f7
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-23 22:32:31 +0530

    Update Java 1D Array (Part 2).java

--- JAVA/Java 1D Array (Part 2).java
+++ JAVA/Java 1D Array (Part 2).java
@@ -22,7 +22,9 @@ public class Solution {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        
         Scanner scan = new Scanner(System.in);
         int q = scan.nextInt();
         while (q-- > 0) {
@@ -30,13 +32,17 @@ public class Solution {
             int leap = scan.nextInt();
             
             int[] game = new int[n];
-            for (int i = 0; i < n; i++) {
+            
+            for (int i = 0; i < n; i++) 
+            {
                 game[i] = scan.nextInt();
             }
 
             System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
         }
+        
         scan.close();
+        
     }
 }
 
