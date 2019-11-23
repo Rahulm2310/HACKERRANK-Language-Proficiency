fa04d7296ec394ff8ad053d607263ccf9d24c6dc
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-23 22:33:24 +0530

    Update Java Hashset.java

--- JAVA/Java Hashset.java
+++ JAVA/Java Hashset.java
@@ -6,23 +6,29 @@ import java.util.regex.*;
 
 public class Solution {
 
- public static void main(String[] args) {
+ public static void main(String[] args) 
+ {
+  
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
         String [] pair_left = new String[t];
         String [] pair_right = new String[t];
         
-        for (int i = 0; i < t; i++) {
+        for (int i = 0; i < t; i++)
+        {
             pair_left[i] = s.next();
             pair_right[i] = s.next();
         }
 
 
 HashSet<String> h=new HashSet<String>();
+  
 for(int i=0;i<t;i++)
 {
+ 
     h.add(pair_left[i]+" "+pair_right[i]);
     System.out.println(h.size());
+ 
 }
 
    }
