b57d7dda58fcb99aee9e6792bf84f71ecdb0ba8a
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-24 23:33:23 +0530

    Update Java Generics.java

--- JAVA/Java Generics.java
+++ JAVA/Java Generics.java
@@ -2,29 +2,39 @@
 import java.io.IOException;
 import java.lang.reflect.Method;
 
+
+
 class Printer
 {
-   /
-   public static <T> void printArray(T element[]){
+   
+   public static <T> void printArray(T element[])
+   {
+      
        for(int i=0;i<element.length;i++)
        {
            System.out.println(element[i]);
        }
+      
    }
 
  
 }
 
+
 public class Solution {
 
 
-    public static void main( String args[] ) {
+    public static void main( String args[] )
+    {
+       
         Printer myPrinter = new Printer();
         Integer[] intArray = { 1, 2, 3 };
         String[] stringArray = {"Hello", "World"};
+       
         myPrinter.printArray(intArray);
         myPrinter.printArray(stringArray);
         int count = 0;
+       
 
         for (Method method : Printer.class.getDeclaredMethods()) {
             String name = method.getName();
@@ -33,6 +43,7 @@ public class Solution {
                 count++;
         }
 
+       
         if(count > 1)System.out.println("Method overloading is not allowed!");
       
     }
