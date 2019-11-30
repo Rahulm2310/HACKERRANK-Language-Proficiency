a88427a91e69c92cfcf4eccd9698d6776c3c2b88
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-01 00:00:07 +0530

    Update Java Iterator.java

--- JAVA/Java Iterator.java
+++ JAVA/Java Iterator.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Main
 {
 	
@@ -6,6 +7,7 @@ public class Main
    {
    
       Iterator it=mylist.iterator();
+	   
       while(it.hasNext())
       {
       
@@ -18,29 +20,38 @@ public class Main
       
    }
    
+	
+	
    @SuppressWarnings({ "unchecked" })
    public static void main(String []args)
    {
+	   
       ArrayList mylist = new ArrayList();
       Scanner sc = new Scanner(System.in);
+	   
       int n = sc.nextInt();
       int m = sc.nextInt();
       
-      for(int i = 0;i<n;i++){
+      for(int i = 0;i<n;i++)
+      {
          mylist.add(sc.nextInt());
       }
       
       mylist.add("###");
+	   
       for(int i=0;i<m;i++){
          mylist.add(sc.next());
       }
       
       
+	   
       Iterator it=func(mylist);
       while(it.hasNext())
       {
+	      
          Object element = it.next();
          System.out.println((String)element);
+	      
       }
       
       
