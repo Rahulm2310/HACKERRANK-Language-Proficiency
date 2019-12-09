20a9e1d76d3c3d3ad6e98a852d3d5148f973bb2d
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-09 23:25:03 +0530

    Update Java Inheritance II.java

--- JAVA/Java Inheritance II.java
+++ JAVA/Java Inheritance II.java
@@ -4,6 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
 class Arithmetic
 {
 
@@ -16,6 +17,7 @@ class Arithmetic
 
 
 
+
 class Adder extends Arithmetic
 {
     
@@ -25,15 +27,17 @@ class Adder extends Arithmetic
 
 public class Solution
 {
-    public static void main(String []args)
+    
+     public static void main(String []args)
     {
     
         Adder a = new Adder();
-        
+
         
         System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
         
-        
+
         System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
+        
      }
 }
