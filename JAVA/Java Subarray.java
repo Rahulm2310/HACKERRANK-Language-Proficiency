efa1ec48426cc0440ea230accf6eaaa27e57d5c7
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-11 23:26:31 +0530

    Update Java Subarray.java

--- JAVA/Java Subarray.java
+++ JAVA/Java Subarray.java
@@ -1,19 +1,24 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Solution {
 
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        
         int arr[]=new int[n];
         int count=0;
         int sum=0;
+        
+        
         for(int i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
         }
+        
 
         for(int i=1;i<=n;i++)
         {
@@ -21,8 +26,10 @@ public class Solution {
             for(int j=0;j<n;j++)
             {
                 sum=0;
+                
                 if(i+j<=n)
                 {
+                    
                     for(int k=j;k<i+j;k++)
                     {
                         sum+=arr[k];
@@ -30,11 +37,16 @@ public class Solution {
 
                     if(sum<0)
                         count++;
+                    
                 }
+                
             }
             
         }
+        
         System.out.println(count);
+        
     }
+    
 }
 
