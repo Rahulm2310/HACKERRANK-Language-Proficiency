e33b8746319fb6a0eaa2d0a51dd71dd709b23f73
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-15 20:00:11 +0530

    Update Java Method Overriding.java

--- JAVA/Java Method Overriding.java
+++ JAVA/Java Method Overriding.java
@@ -1,26 +1,38 @@
 import java.util.*;
+
+
+
 class Sports{
+	
 
     String getName()
     {
         return "Generic Sports";
     }
   
+	
+	
     void getNumberOfTeamMembers()
     {
         System.out.println( "Each team has n players in " + getName() );
     }
+	
+	
 }
+
+
 
 
 
 class Soccer extends Sports
 {
+
     @Override
     String getName()
     {
         return "Soccer Class";
     }
+
 
 
     void getNumberOfTeamMembers()
@@ -37,12 +49,14 @@ public class Solution
 	
     public static void main(String []args)
     {
+	    
         Sports c1 = new Sports();
         Soccer c2 = new Soccer();
         System.out.println(c1.getName());
         c1.getNumberOfTeamMembers();
         System.out.println(c2.getName());
         c2.getNumberOfTeamMembers();
+	    
         
 	}
 }
