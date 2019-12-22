98a27964549b65ee492348804d691c448ad8e81f
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-22 19:00:25 +0530

    Update Covariant Return Types.java

--- JAVA/Covariant Return Types.java
+++ JAVA/Covariant Return Types.java
@@ -2,39 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+
 class Flower {
 }
+
+
 
 class Jasmine {
 }
 
+
 class Lily {
 }
+
 
 class Region {
 }
 
+
 class WestBengal {
 }
+
 
 class AndhraPradesh {
 }
 
 
+
+
 public class Solution {
+  
   public static void main(String[] args) throws IOException {
+    
       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
       String s = reader.readLine().trim();
       Region region = null;
+    
       switch (s) {
+          
         case "WestBengal":
           region = new WestBengal();
           break;
+          
         case "AndhraPradesh":
           region = new AndhraPradesh();
           break;
       }
+    
+    
       Flower flower = region.yourNationalFlower();
       System.out.println(flower.whatsYourName());
+    
+    
     }
 }
