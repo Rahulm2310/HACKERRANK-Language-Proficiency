2613253c50a2ee2cee5821e8e1d040965c6362b5
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-26 20:03:21 +0530

    Update Java Currency Formatter.java

--- JAVA/Java Currency Formatter.java
+++ JAVA/Java Currency Formatter.java
@@ -4,9 +4,14 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+
 public class Solution {
     
+    
     public static void main(String[] args) {
+        
+        
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
         scanner.close();
@@ -15,6 +20,7 @@ public class Solution {
         Locale INDIA = new Locale("en", "IN");
         
 
+        
         NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
         NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
         NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
@@ -25,6 +31,8 @@ public class Solution {
         System.out.println("India: " + india.format(payment));
         System.out.println("China: " + china.format(payment));
         System.out.println("France: " + france.format(payment));
+        
+        
         
     }
 }
