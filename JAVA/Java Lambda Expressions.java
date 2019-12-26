d0b87311c7dbed6794ec4e0ee1f6587deeaff37d
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-26 20:08:01 +0530

    Update Java Lambda Expressions.java

--- JAVA/Java Lambda Expressions.java
+++ JAVA/Java Lambda Expressions.java
@@ -2,40 +2,52 @@ import java.io.*;
 import java.util.*;
 
 
+
 interface PerformOperation 
 {
+     
      boolean check(int a);
+     
 }
 
 
 class MyMath {
  
- public static boolean checker(PerformOperation p, int num) {
+     
+     public static boolean checker(PerformOperation p, int num)
+     {
   
-  return p.check(num);
+     return p.check(num);
   
- }
+     }
 
  
-    public PerformOperation isOdd(){
+     
+    public PerformOperation isOdd()
+    {
      
         return (int a) -> a % 2 != 0;
      
     }
  
     
-    public PerformOperation isPrime(){
+    public PerformOperation isPrime()
+    {
      
         return (int a) -> java.math.BigInteger.valueOf(a).isProbablePrime(1);
+         
     }
  
  
     
-    public PerformOperation isPalindrome(){
+    public PerformOperation isPalindrome()
+    {
      
         return (int a) ->  Integer.toString(a).equals( new StringBuilder(Integer.toString(a)).reverse().toString() );
+         
     }
  
+     
 }
 
 
@@ -46,9 +58,12 @@ public class Solution {
                  public static void main(String[] args) throws IOException
                  {
                   
+                      
                   MyMath ob = new MyMath();
                   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                   int T = Integer.parseInt(br.readLine());
+                      
+                      
                   PerformOperation op;
                   boolean ret = false;
                   String ans = null;
@@ -57,37 +72,39 @@ public class Solution {
                   while (T--> 0) 
                   {
                    
-                   String s = br.readLine().trim();
-                   StringTokenizer st = new StringTokenizer(s);
-                   int ch = Integer.parseInt(st.nextToken());
-                   int num = Integer.parseInt(st.nextToken());
-                   
-                   
-                   if (ch == 1) {
-                   
-                    op = ob.isOdd();
-                    ret = ob.checker(op, num);
-                    ans = (ret) ? "ODD" : "EVEN";
-                    
-                    
-                   } else if (ch == 2) {
-                   
-                    op = ob.isPrime();
-                    ret = ob.checker(op, num);
-                    ans = (ret) ? "PRIME" : "COMPOSITE";
-                    
-                    
-                   } else if (ch == 3) {
-                    op = ob.isPalindrome();
-                    ret = ob.checker(op, num);
-                    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+                             String s = br.readLine().trim();
+                             StringTokenizer st = new StringTokenizer(s);
+                             int ch = Integer.parseInt(st.nextToken());
+                             int num = Integer.parseInt(st.nextToken());
 
-                    
-                   }
-                   
-                   System.out.println(ans);
-                   
-                   
+
+                             if (ch == 1) {
+
+                              op = ob.isOdd();
+                              ret = ob.checker(op, num);
+                              ans = (ret) ? "ODD" : "EVEN";
+
+
+                             } else if (ch == 2) {
+
+                              op = ob.isPrime();
+                              ret = ob.checker(op, num);
+                              ans = (ret) ? "PRIME" : "COMPOSITE";
+
+
+                             } else if (ch == 3) {
+                              op = ob.isPalindrome();
+                              ret = ob.checker(op, num);
+                              ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+
+                             }
+
+                             System.out.println(ans);
+
+
                   }
+                      
                  }
+     
 }
