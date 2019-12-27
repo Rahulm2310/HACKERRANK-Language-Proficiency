c76aaa319a6fcdf55f09b78b16d45fca334dd60d
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-27 23:37:03 +0530

    Update Java Anagrams.java

--- JAVA/Java Anagrams.java
+++ JAVA/Java Anagrams.java
@@ -1,29 +1,42 @@
 import java.util.Scanner;
 
+
 public class Solution {
+    
+    
 
     static boolean isAnagram(String a, String b) {
        
+        
         List<Character> A=new ArrayList<Character>();
         List<Character> B=new ArrayList<Character>();
         int flag=0;
+        
+        
 
         if(a.length()==b.length())
         {
 
             for(int i=0;i<a.length();i++)
             {
+                
                  A.add(a.charAt(i).toLowerCase());
 
                  B.add(b.charAt(i).toLowerCase());
+                
             }
 
+            
 
             for(int k=0;k<A.size();k++)
             {
+                
                 if(Collections.frequency(A,A.get(i))!=Collections.frequency(B,A.get(i))){
+                    
                     flag=1;
+                    
                 }
+                
             }
             
 
@@ -32,10 +45,12 @@ public class Solution {
                 return true;
             }
             
+            
             else if(flag==1)
             {
                 return false;
             }
+            
             
         }
 
@@ -43,6 +58,8 @@ public class Solution {
         {
             return false;
         }
+        
+        
 
 
 
@@ -57,8 +74,14 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String a = scan.next();
         String b = scan.next();
+      
         scan.close();
+      
         boolean ret = isAnagram(a, b);
+      
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+      
+      
     }
+    
 }
