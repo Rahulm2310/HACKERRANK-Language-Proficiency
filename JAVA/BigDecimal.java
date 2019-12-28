76522013a9415ac6170b01b8d33e874462cc8b62
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-28 16:50:33 +0530

    Update BigDecimal.java

--- JAVA/BigDecimal.java
+++ JAVA/BigDecimal.java
@@ -1,16 +1,28 @@
 import java.math.BigDecimal;
 import java.util.*;
+
+
 class Solution{
+    
+    
     
     public static void main(String []args)
     {
         
+        
         Scanner sc= new Scanner(System.in);
+        
         int n=sc.nextInt();
         String []s=new String[n+2];
+        
+        
         for(int i=0;i<n;i++){
+            
             s[i]=sc.next();
+            
         }
+        
+        
         sc.close();
 
         
@@ -21,9 +33,12 @@ class Solution{
     @Override
     public int compare(String a1, String a2) {
         
+        
         BigDecimal a = new BigDecimal(a1);
         BigDecimal b = new BigDecimal(a2);
         return a.compareTo(b);
+        
+        
     }
 }));
         
@@ -32,7 +47,12 @@ class Solution{
         //Output
         for(int i=0;i<n;i++)
         {
+            
+            
             System.out.println(s[i]);
+            
         }
+        
     }
+    
 }
